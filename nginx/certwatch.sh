421988bf059937d4d98191fe89b517d75ef397cf
proxymurder <30883472+proxymurder@users.noreply.github.com>
2023-08-07 21:50:43 -0600

    EOL

--- nginx/certwatch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-while true; do
-    inotifywait -e modify ${STEPPATH}/site.crt
-    nginx -s reload
-done
--- step/renewer/renewer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-step ca renew --force /var/local/step/site.crt /var/local/step/site.key
